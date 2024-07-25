50570bd55280a34776d36d14c67a55851911488e
naledi-dikgale <naledi22khosi@gmail.com>
2024-07-25 19:51:08 +0200

    Project structure

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { ThemeProvider } from './ThemeContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider>
+        <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
