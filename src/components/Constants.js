e03d68ab5499151c4991025ebfa5c4129f8f2e70
naledi-dikgale <naledi22khosi@gmail.com>
2024-07-25 19:52:15 +0200

    Add carousel

---
+++ src/components/Constants.js
@@ -0,0 +1,24 @@
+const FAQ_DATA = [
+  {
+    question: 'What is an audiologist?',
+    answer: 'An audiologist is a healthcare professional who specializes in identifying, diagnosing, treating, and monitoring disorders of the auditory and vestibular system portions of the ear.'
+  },
+  {
+    question: 'When do I need a hearing assessment?',
+    answer: 'If your answer is YES to one of more of the following questions, a hearing assessment is recommended: I often have to ask people to repeat themselves. I can hear people talking, but cannot make out what they are saying. It is difficult to follow conversations in the presence of background noise (like restaurants) or in crowds. I find it difficult to hear the telephone ring. I battle to hear clearly on a telephone. I need to turn up the volume of the TV and/or radio. I have continuous ringing in my ears. I often feel dizzy /vertigo.'
+  },
+  {
+    question: 'How often must my hearing be assessed?',
+    answer: 'Once a year, especially if you are over 50 years of age. If you are younger than 50 years of age, every 2 years is recommended'
+  },
+  {
+    question: 'Will medical aid pay for a hearing test?',
+    answer: 'Most medical aids will pay for a hearing test, but it is best to check with your medical aid to confirm.'
+  },
+  {
+    question: 'How much do hearing aids cost?',
+    answer: 'Hearing aids are available in a wide range of prices. The cost of a hearing aid depends on the technology level, the style of the hearing aid, and the features that are required. We offer a wide range of hearing aids to suit all budgets. We also offer payment plans to make hearing aids more affordable. We will discuss all the options with you during your appointment.'
+  },
+];
+
+export default FAQ_DATA;
